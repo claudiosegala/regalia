84facf875e26c6342cb0e8f16ade06f4d0476dc1
claudiosegala <claudiosegalafilho@gmail.com>
2019-05-29 15:52:14 -0300

    stuff

--- regalia/include/SpriteSheetData.h
+++ regalia/include/SpriteSheetData.h
@@ -4,12 +4,16 @@
 
 class SpriteSheetData {
 public:
-	SpriteSheetData(const std::string& file, int imageWidth, int imageHeight, float frameTime, int totalAnimations, const std::map<int, int>& animationsFrames, bool selfDestruct = false);
-	SpriteSheetData(const SpriteSheetData& other) = delete;
 
 	std::string file;
+	
 	float frameTime;
+	
 	bool selfDestruct;
+	
+	SpriteSheetData(const std::string& file, int imageWidth, int imageHeight, float frameTime, int totalAnimations, const std::map<int, int>& animationsFrames, bool selfDestruct = false);
+	
+	SpriteSheetData(const SpriteSheetData& other) = delete;
 
 	inline SDL_Rect GetAnimationRect(int animationId, int frame) const {
 		assert(animationId >= 0 && animationId < int(animationsRect.size()));
