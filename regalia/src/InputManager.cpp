16a678bc2db87cdef231a79efa2995b596bc923c
Ricardo <ricardokury@outlook.com>
2019-07-06 18:22:58 -0300

    Haptic gamby

--- regalia/src/InputManager.cpp
+++ regalia/src/InputManager.cpp
@@ -193,7 +193,7 @@ void InputManager::TreatEvent(SDL_Event& event) {
 }
 
 void InputManager::LoadControllers() {
-	for (int i = 0; i < SDL_NumJoysticks(); i++) {
+	for (int i = SDL_NumJoysticks() - 1; i >= 0; i--) {
 		if (SDL_IsGameController(i)) {
 			controllers.emplace_back(i);
 		}
