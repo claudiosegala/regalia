d009adb6b35ea4f1291bb21ece9b49cd4f0bcc1a
Ricardo <ricardokury@outlook.com>
2019-07-08 18:49:22 -0300

    Adds missing file from last commit

--- regalia/src/PlayState.cpp
+++ regalia/src/PlayState.cpp
@@ -63,7 +63,7 @@ void PlayState::Update(unsigned dt) {
 	GameData::CurrentRoundTimer.Update(dt);
 
 	// TODO: change to Player::counter < 2
-	if (Player::counter == 0) {
+	if (player_count == 1) {
 		GameData::Set++;
 		GameData::Finished = (GameData::Set == Constants::Game::Sets);
 		LoadScoreState();
